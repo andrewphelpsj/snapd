3e40380c16cfb8336ae4d8e9afd2e04012728f6f
Andrew Phelps <andrew.phelps@canonical.com>
2024-06-04 13:00:19 -0400

    o/snapstate: rename RequireOneSnap to ExpectOneSnap

--- overlord/snapstate/snapstate_install_test.go
+++ overlord/snapstate/snapstate_install_test.go
@@ -6081,7 +6081,7 @@ func (s *snapmgrTestSuite) TestInstallFromStoreOneSnap(c *C) {
 	)
 
 	_, _, err := snapstate.InstallOne(context.Background(), s.state, t, snapstate.Options{
-		RequireOneSnap: true,
+		ExpectOneSnap: true,
 	})
 	c.Check(err, testutil.ErrorIs, snapstate.ErrExpectedOneSnap)
 
@@ -6101,7 +6101,7 @@ func (s *snapmgrTestSuite) TestInstallOneSnapMisbehavingGoal(c *C) {
 	}
 
 	_, _, err := snapstate.InstallOne(context.Background(), s.state, &goal, snapstate.Options{
-		RequireOneSnap: true,
+		ExpectOneSnap: true,
 	})
 	c.Check(err, testutil.ErrorIs, snapstate.ErrExpectedOneSnap)
 }
