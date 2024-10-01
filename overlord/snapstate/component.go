d1a9ea71da3ebd5b77126177c5eaf85a2b4b9ec2
Andrew Phelps <andrew.phelps@canonical.com>
2024-10-01 18:13:54 -0400

    o/snapstate: resolve component errors when resolving validation sets

--- overlord/snapstate/component.go
+++ overlord/snapstate/component.go
@@ -57,14 +57,24 @@ func InstallComponents(
 		return nil, err
 	}
 
-	for _, comp := range names {
-		if snapst.CurrentComponentSideInfo(naming.NewComponentRef(info.SnapName(), comp)) != nil {
-			return nil, snap.AlreadyInstalledComponentError{Component: comp}
-		}
-	}
-
 	if vsets == nil {
-		vsets = snapasserts.NewValidationSets()
+		// we only check for already installed components when no validation
+		// sets are provided, since this will allow us to refresh and install
+		// new components at the same time when resolving validation sets
+		for _, comp := range names {
+			if snapst.CurrentComponentSideInfo(naming.NewComponentRef(info.SnapName(), comp)) != nil {
+				return nil, snap.AlreadyInstalledComponentError{Component: comp}
+			}
+		}
+
+		if opts.Flags.IgnoreValidation {
+			vsets = snapasserts.NewValidationSets()
+		} else {
+			vsets, err = EnforcedValidationSets(st)
+			if err != nil {
+				return nil, err
+			}
+		}
 	}
 
 	compsups, err := componentSetupsForInstall(ctx, st, names, snapst, RevisionOptions{
