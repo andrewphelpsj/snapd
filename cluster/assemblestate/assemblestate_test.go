d276d2c37c443fad51102014c29fce11c6b7ce24
Andrew Phelps <andrew.phelps@canonical.com>
2025-08-29 12:36:02 -0400

    fixup

--- cluster/assemblestate/assemblestate_test.go
+++ cluster/assemblestate/assemblestate_test.go
@@ -121,7 +121,7 @@ func (s *assembleSuite) TestRun(c *check.C) {
 		wg.Add(1)
 		go func() {
 			defer wg.Done()
-			_, err := as.Run(
+			_, _, err := as.Run(
 				ctx,
 				listeners[rdt],
 				assemblestate.NewHTTPSTransport(),
@@ -157,7 +157,7 @@ func (s *assembleSuite) TestRun(c *check.C) {
 	disco := make(chan []string, 1)
 	disco <- addrs
 
-	routes, err := as.Run(
+	_, routes, err := as.Run(
 		ctx,
 		listeners[rdt],
 		assemblestate.NewHTTPSTransport(),
