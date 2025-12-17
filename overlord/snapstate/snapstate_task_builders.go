e7f98d9063d45493ed28f13e4671670c6091abe3
Andrew Phelps <andrew.phelps@canonical.com>
2025-12-17 13:27:00 -0500

    o/snapstate: add new builder and span types to help track ranges of tasks during generation

---
+++ overlord/snapstate/snapstate_task_builders.go
@@ -0,0 +1,133 @@
+// -*- Mode: Go; indent-tabs-mode: t -*-
+
+/*
+ * Copyright (C) 2026 Canonical Ltd
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package snapstate
+
+import "github.com/snapcore/snapd/overlord/state"
+
+// builder constructs a graph of tasks with automatic dependency chaining and
+// metadata management.
+type builder struct {
+	// ts contains all tasks managed by this builder and any child span.
+	// Primarily, this is used to keep track of edges.
+	ts *state.TaskSet
+	// tail points to the tip of the current graph. It is updated by the builder
+	// or any child spans.
+	tail *state.Task
+	// meta contains data that tasks added to the graph get adorned with.
+	meta map[string]any
+}
+
+// newBuilder returns a builder initialized with an empty task set.
+func newBuilder() builder {
+	return builder{
+		ts: state.NewTaskSet(),
+	}
+}
+
+// TaskSet returns the task set that contains all tasks added to this builder,
+// either directly or via child spans.
+func (b *builder) TaskSet() *state.TaskSet {
+	return b.ts
+}
+
+// Add appends a task to the end of the existing chain of tasks. Any existing
+// metadata is attached to the given task.
+func (b *builder) Add(t *state.Task) {
+	tmp := span{b: b}
+	tmp.Add(t)
+}
+
+// NewSpan creates a new span that shares this builder's task set and tail.
+func (b *builder) NewSpan() span {
+	return span{b: b}
+}
+
+// span represents a logical grouping of tasks within a task builder. This type
+// is used to contruct ranges of tasks for easier grouping, while still enabling
+// the marking of edges in the parent builder's task set.
+type span struct {
+	b     *builder
+	tasks []*state.Task
+}
+
+// SetMetadata sets the metadata applied to all future tasks added to the parent
+// builder's task set.
+func (s *span) SetMetadata(meta map[string]any) {
+	s.b.meta = meta
+}
+
+// Add appends a task to graph of tasks managed by the parent builder.
+// Additionally, the task is added to this span's range of tasks. The task has
+// the builder's metadata applied.
+func (s *span) Add(t *state.Task) {
+	for k, v := range s.b.meta {
+		t.Set(k, v)
+	}
+	s.AddWithoutMetadata(t)
+}
+
+// AddWithoutMetadata behaves the same as Add, but metadata is not applied to
+// the added task.
+func (s *span) AddWithoutMetadata(t *state.Task) {
+	if s.b.tail != nil {
+		t.WaitFor(s.b.tail)
+	}
+	s.b.tail = t
+	s.b.ts.AddTask(t)
+	s.tasks = append(s.tasks, t)
+}
+
+// Splice chains a task into the dependency sequence without adding it to the
+// builder's task set or span. This is useful when inserting a task into the
+// builder's graph that might be shared by multiple builders.
+func (s *span) Splice(t *state.Task) {
+	if s.b.tail != nil {
+		t.WaitFor(s.b.tail)
+	}
+	s.b.tail = t
+}
+
+// UpdateEdge marks the task as an edge. If the task set owned by the parent
+// builder already has that edge, it is overwritten.
+func (s *span) UpdateEdge(t *state.Task, e state.TaskSetEdge) {
+	s.b.ts.MarkEdge(t, e)
+}
+
+// AddTSWithoutMeta adds all tasks from another task set without applying
+// metadata. It is assumed that the last task in the task set is the final task
+// in it's dependency graph.
+func (s *span) AddTSWithoutMeta(ts *state.TaskSet) {
+	tasks := ts.Tasks()
+	if len(tasks) == 0 {
+		return
+	}
+
+	if s.b.tail != nil {
+		ts.WaitFor(s.b.tail)
+	}
+	s.b.ts.AddAll(ts)
+	s.b.tail = tasks[len(tasks)-1]
+	s.tasks = append(s.tasks, ts.Tasks()...)
+}
+
+// Tasks returns the tasks owned by this span
+func (s *span) Tasks() []*state.Task {
+	return s.tasks
+}
---
+++ overlord/snapstate/snapstate_task_builders_internal_test.go
@@ -0,0 +1,268 @@
+// -*- Mode: Go; indent-tabs-mode: t -*-
+
+/*
+ * Copyright (C) 2026 Canonical Ltd
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package snapstate
+
+import (
+	. "gopkg.in/check.v1"
+
+	"github.com/snapcore/snapd/overlord/state"
+)
+
+type taskBuilderTestSuite struct{}
+
+var _ = Suite(&taskBuilderTestSuite{})
+
+func (s *taskBuilderTestSuite) TestAddWithMetadata(c *C) {
+	st := state.New(nil)
+	st.Lock()
+	defer st.Unlock()
+
+	b := newBuilder()
+	span := b.NewSpan()
+
+	// this metadata will be applied to all tasks added via this builder or any
+	// child spans
+	span.SetMetadata(map[string]any{"snap-setup": "snapsup-task"})
+
+	// Add applies the builder's metadata and chains the task to the tail
+	t1 := st.NewTask("task-1", "test")
+	span.Add(t1)
+
+	var snapsup string
+	c.Assert(t1.Get("snap-setup", &snapsup), IsNil)
+	c.Check(snapsup, Equals, "snapsup-task")
+
+	c.Check(span.Tasks(), DeepEquals, []*state.Task{t1})
+	c.Check(b.TaskSet().Tasks(), DeepEquals, []*state.Task{t1})
+
+	// Add applies the builder's metadata and chains the task to the tail. note,
+	// this is added directly on the builder, so this task should not be a part
+	// of the span.
+	t2 := st.NewTask("task-2", "test")
+	b.Add(t2)
+
+	snapsup = ""
+	c.Assert(t2.Get("snap-setup", &snapsup), IsNil)
+	c.Check(snapsup, Equals, "snapsup-task")
+
+	c.Check(t2.WaitTasks(), DeepEquals, []*state.Task{t1})
+	c.Check(span.Tasks(), DeepEquals, []*state.Task{t1})
+	c.Check(b.TaskSet().Tasks(), DeepEquals, []*state.Task{t1, t2})
+}
+
+func (s *taskBuilderTestSuite) TestSpanAddWithoutMetadata(c *C) {
+	st := state.New(nil)
+	st.Lock()
+	defer st.Unlock()
+
+	b := newBuilder()
+
+	span := b.NewSpan()
+	span.SetMetadata(map[string]any{"snap-setup": "snapsup-task"})
+
+	task := st.NewTask("task-1", "test")
+
+	// skips adding metadata but still chains the task
+	span.AddWithoutMetadata(task)
+
+	var snapsup string
+	c.Check(task.Get("snap-setup", &snapsup), Not(IsNil))
+	c.Check(snapsup, Equals, "")
+
+	c.Check(b.TaskSet().Tasks(), DeepEquals, []*state.Task{task})
+	c.Check(span.Tasks(), DeepEquals, []*state.Task{task})
+}
+
+func (s *taskBuilderTestSuite) TestSpanAddChaining(c *C) {
+	st := state.New(nil)
+	st.Lock()
+	defer st.Unlock()
+
+	b := newBuilder()
+	span := b.NewSpan()
+
+	first := st.NewTask("task-1", "first")
+	span.Add(first)
+
+	second := st.NewTask("task-2", "second")
+
+	// each task waits for the previous task in the chain
+	span.Add(second)
+
+	c.Check(first.WaitTasks(), HasLen, 0)
+	c.Check(second.WaitTasks(), DeepEquals, []*state.Task{first})
+
+	// span.tasks tracks all tasks added to this span, in order
+	c.Check(span.Tasks(), DeepEquals, []*state.Task{first, second})
+}
+
+func (s *taskBuilderTestSuite) TestSpanSplice(c *C) {
+	st := state.New(nil)
+	st.Lock()
+	defer st.Unlock()
+
+	b := newBuilder()
+	span := b.NewSpan()
+
+	first := st.NewTask("task-1", "first")
+	span.Add(first)
+	second := st.NewTask("task-2", "second")
+
+	// splice chains the task but does not add it to the builder or the span
+	span.Splice(second)
+
+	// second waits for first but is not kept around in the builder or the span
+	c.Check(second.WaitTasks(), DeepEquals, []*state.Task{first})
+	c.Check(b.TaskSet().Tasks(), DeepEquals, []*state.Task{first})
+	c.Check(span.Tasks(), DeepEquals, []*state.Task{first})
+}
+
+func (s *taskBuilderTestSuite) TestSpliceSharedTask(c *C) {
+	st := state.New(nil)
+	st.Lock()
+	defer st.Unlock()
+
+	b1 := newBuilder()
+	span1 := b1.NewSpan()
+
+	t1 := st.NewTask("task-1", "in-builder-1")
+	span1.Add(t1)
+
+	b2 := newBuilder()
+	span2 := b2.NewSpan()
+
+	t2 := st.NewTask("task-2", "in-builder-2")
+	span2.Add(t2)
+
+	// splice adds the same task to both chains
+	spliced := st.NewTask("spliced", "in-both")
+	span1.Splice(spliced)
+	span2.Splice(spliced)
+
+	// spliced now waits for both task1 and task3, belonging to multiple chains
+	c.Check(spliced.WaitTasks(), HasLen, 2)
+	c.Check(spliced.WaitTasks()[0], Equals, t1)
+	c.Check(spliced.WaitTasks()[1], Equals, t2)
+
+	// but it doesn't belong to either builder task sets. this lets callers
+	// safely add the generated task sets to the same change, since a change
+	// cannot contain a task more than once.
+	c.Check(b1.TaskSet().Tasks(), DeepEquals, []*state.Task{t1})
+	c.Check(b2.TaskSet().Tasks(), DeepEquals, []*state.Task{t2})
+}
+
+func (s *taskBuilderTestSuite) TestSpanUpdateEdge(c *C) {
+	st := state.New(nil)
+	st.Lock()
+	defer st.Unlock()
+
+	b := newBuilder()
+	span := b.NewSpan()
+
+	first := st.NewTask("task-1", "first")
+	span.Add(first)
+
+	edge := state.TaskSetEdge("begin-edge")
+	span.UpdateEdge(first, edge)
+
+	edgeTask := b.TaskSet().MaybeEdge(edge)
+	c.Check(edgeTask, Equals, first)
+
+	second := st.NewTask("task-2", "second")
+	span.Add(second)
+
+	// edges can be overwritten with a different task
+	span.UpdateEdge(second, edge)
+
+	edgeTask = b.TaskSet().MaybeEdge(edge)
+	c.Check(edgeTask, Equals, second)
+}
+
+func (s *taskBuilderTestSuite) TestSpanAddTSWithoutMeta(c *C) {
+	st := state.New(nil)
+	st.Lock()
+	defer st.Unlock()
+
+	b := newBuilder()
+	span := b.NewSpan()
+
+	// add an empty task set, just to make sure we don't panic
+	span.AddTSWithoutMeta(state.NewTaskSet())
+
+	first := st.NewTask("task-1", "first")
+	span.Add(first)
+
+	second := st.NewTask("task-2", "second")
+	third := st.NewTask("task-3", "third")
+	third.WaitFor(second)
+
+	// AddTSWithoutMeta adds an entire TaskSet, preserving its internal
+	// dependencies
+	otherTS := state.NewTaskSet(second, third)
+	span.AddTSWithoutMeta(otherTS)
+
+	fourth := st.NewTask("task-4", "fourth")
+	span.Add(fourth)
+
+	// third and second both wait for first, and third waits for second (the
+	// original chain within the task set)
+	c.Check(second.WaitTasks(), DeepEquals, []*state.Task{first})
+	c.Check(third.WaitTasks(), DeepEquals, []*state.Task{second, first})
+
+	// fourth waits on the tail of the added task set, third
+	c.Check(fourth.WaitTasks(), DeepEquals, []*state.Task{third})
+
+	// all tasks are contained within the builder and span
+	c.Check(b.TaskSet().Tasks(), DeepEquals, []*state.Task{first, second, third, fourth})
+	c.Check(span.Tasks(), DeepEquals, []*state.Task{first, second, third, fourth})
+}
+
+func (s *taskBuilderTestSuite) TestMultipleSpansShareBuilder(c *C) {
+	st := state.New(nil)
+	st.Lock()
+	defer st.Unlock()
+
+	b := newBuilder()
+
+	span1 := b.NewSpan()
+
+	first := st.NewTask("task-1", "first")
+	span1.Add(first)
+
+	span2 := b.NewSpan()
+
+	second := st.NewTask("task-2", "second")
+	span2.Add(second)
+
+	third := st.NewTask("task-3", "third")
+	span2.Add(third)
+
+	c.Check(first.WaitTasks(), HasLen, 0)
+
+	// both spans share the same task set and tail, forming a single chain
+	c.Check(second.WaitTasks(), DeepEquals, []*state.Task{first})
+	c.Check(third.WaitTasks(), DeepEquals, []*state.Task{second})
+	c.Check(b.TaskSet().Tasks(), DeepEquals, []*state.Task{first, second, third})
+
+	// each span tracks only the tasks it added, enabling callers to keep track
+	// of ranges
+	c.Check(span1.Tasks(), DeepEquals, []*state.Task{first})
+	c.Check(span2.Tasks(), DeepEquals, []*state.Task{second, third})
+}
